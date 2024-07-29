d0d77a81b4e569f3f488e00290af5444831fd54a
Alireza <mrarshax@gmail.com>
2024-07-29 16:59:04 +0330

    The project was created

---
+++ Source/DeadMage/DeadMage.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DeadMage.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DeadMage, "DeadMage" );
